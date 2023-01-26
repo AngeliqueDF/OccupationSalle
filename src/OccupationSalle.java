6c26b5925fdbb409144fb73a282c7ec285d74d31
angelique-df <faye.angelique.d@gmail.com>
2023-01-26 18:25:09 +0000

    Create OccupationSalle.java

---
+++ src/OccupationSalle.java
@@ -0,0 +1,11 @@
+public class OccupationSalle {
+    public static void main(String[] args) throws InterruptedException {
+        boolean[][] occupee = {
+                {false, true, true, true, false, true, true, false, true, false},
+                {false, true, true, true, false, true, true, false, true, false},
+                {false, false, true, true, false, false, true, false, true, false},
+                {true, true, false, true, false, true, true, false, true, true},
+                {false, true, false, true, false, true, true, false, false, false}
+        };
+    }
+};
